8637c86b87ae9e91a64660271f9dccc53599dcf3
HILAYTRIVEDI <hilaytrivedi.ht@gmail.com>
2020-11-06 20:41:18 +0530

    Updated

--- Amazon/Flattern_Binary_Tree.py
+++ Amazon/Flattern_Binary_Tree.py
@@ -8,6 +8,49 @@ class Node:
         return f"({self.value}, {self.left}, {self.right})"
 
 
+'''
+#############################
+##### Dynamic Solution ######
+#############################
+'''
+
+
+def flatten_bst(root):
+    # Check if the node is leaf or not
+    if root.left == None and root.right == None:
+        return root
+    # If only right child is avaliable
+    elif root.left == None:
+        root.right = flatten_bst(root.right)
+        return root
+    # If only left child is avaliable swap it with right child and call recurssion
+    elif root.right == None:
+        root.right = root.left
+        root.left = None
+        root.right = flatten_bst(root.right)
+        return root
+    else:
+        temp_Left = root.left
+        temp_Right = root.right
+        # Finding right end of left child
+        while temp_Left.right != None:
+            temp_Left = temp_Left.right
+
+        temp_Left.right = temp_Right
+        root.right = temp_Left
+        root.left = None
+
+        root.right = flatten_bst(root.right)
+        return root
+
+
+'''
+#############################
+##### Static Solution ######
+#############################
+'''
+
+
 def flatten_bst(root):
     # Save the whole right sub tree in the variable
     temp = root.right
